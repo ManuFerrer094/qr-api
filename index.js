722c44a7f2c0e808483d77113ba545c4437b312d
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-07 10:09:11 +0100

    Base code

--- index.js
+++ index.js
@@ -19,36 +19,6 @@ app.use(cors());
 // Endpoint para generar código QR para URL
 app.post('/generate/url', qrController.generateUrlQR);
 
-// Endpoint para generar código QR para vCard
-app.post('/generate/vcard', qrController.generateVCardQR);
-
-// Endpoint para generar código QR para Twitter
-app.post('/generate/twitter', qrController.generateTwitterQR);
-
-// Endpoint para generar código QR para correo electrónico
-app.post('/generate/email', qrController.generateEmailQR);
-
-// Endpoint para generar código QR para SMS
-app.post('/generate/sms', qrController.generateSMSQR);
-
-// Endpoint para generar código QR para WiFi
-app.post('/generate/wifi', qrController.generateWiFiQR);
-
-// Endpoint para generar código QR para Bitcoin
-app.post('/generate/bitcoin', qrController.generateBitcoinQR);
-
-// Endpoint para generar código QR para archivos PDF
-app.post('/generate/pdf', qrController.generatePDFQR);
-
-// Endpoint para generar código QR para archivos MP3
-app.post('/generate/mp3', qrController.generateMP3QR);
-
-// Endpoint para generar código QR para la App Store
-app.post('/generate/app-store', qrController.generateAppStoreQR);
-
-// Endpoint para generar código QR para la Galería de imágenes
-app.post('/generate/image-gallery', qrController.generateImageGalleryQR);
-
 // Endpoint original para generar código QR
 app.post('/generate', qrController.generateQR);
 
