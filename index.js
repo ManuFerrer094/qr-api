dde8dc6583c34de5f441ece28c94dc0acd1f652d
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-08 07:11:14 +0100

    Users

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// index.js
+
 const express = require('express');
+const mongoose = require('mongoose');
 const rateLimit = require("express-rate-limit");
 const cors = require('cors');
 const qrController = require('./controllers/qrController');
+const userController = require('./controllers/userController');
 const fs = require('fs');
 
 const app = express();
@@ -17,15 +21,27 @@ app.use(limiter);
 app.use(express.json());
 app.use(cors());
 
-// Endpoint para generar código QR para URL
+mongoose.connect('mongodb://localhost:27017/myapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexión a la base de datos exitosa');
+}).catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+});
+
+//Endpoints para los users
+app.post('/users', userController.createUser);
+app.get('/users/:userId', userController.getUser);
+app.put('/users/:userId', userController.updateUser);
+app.delete('/users/:userId', userController.deleteUser);
+
+//Endpoints para los qr
 app.post('/generate/url', qrController.generateUrlQR);
-
-// Endpoint para generar código QR para contacto
 app.post('/generate/vcard', qrController.generateContactQR);
-
-// Endpoint original para generar código QR
 app.post('/generate', qrController.generateQR);
 
+//Documentacion
 app.get('/', (req, res) => {
     fs.readFile('docs/documentation.html', 'utf8', (err, data) => {
         if (err) {
