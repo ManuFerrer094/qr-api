b4f6f1b2681ff5917cd07ac38d8560f27cfac6ab
Manuel <manuferrer094@gmail.com>
2024-02-11 10:44:32 +0100

    Style upgrade

--- index.js
+++ index.js
@@ -22,99 +22,146 @@ app.post('/generate/vcard', qrController.generateContactQR);
 app.post('/generate', qrController.generateQR);
 
 app.get('/', (req, res) => {
-    // Contenido HTML a servir
     const htmlContent = `
     <!DOCTYPE html>
     <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Documentación de la API de Generación de Códigos QR</title>
-        <link rel="stylesheet" href="./resources/styles.css">
+        <title>QR API</title>
+        <link rel="icon" href="qrfavicon.ico" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Honk&family=Play:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Honk&family=Inter:wght@100..900&family=Play:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
     <style>
+    :root {
+        --font-family: "Inter", sans-serif;
+        --heading-font-family: "Play", sans-serif;
+        --primary-color: #00c8ff;
+        --secondary-color: #12cf3e;
+        --text-color: #ddd;
+        --background-color: #333;
+        --code-background-color: #1e1e1e;
+        --code-text-color: #fff;
+        --link-hover-color: #0056b3;
+    }
+    
     body {
-        font-family: Arial, sans-serif;
+        font-family: var(--font-family);
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+        font-variation-settings: "slnt" 0;
         line-height: 1.6;
         margin: 20px;
         padding: 20px;
+        background-color: var(--background-color);
+        color: var(--text-color);
+    }
+    
+    h1, h2, h3, h4 {
+        font-family: var(--heading-font-family);
+        font-weight: 700;
+        font-style: normal;
     }
     
     h1 {
-        color: #333;
+        font-size: 2.5em;
     }
     
     h2 {
-        color: #555;
+        font-size: 2em;
+    }
+    
+    h3 {
+        font-size: 1.5em;
+    }
+    
+    h4 {
+        font-size: 1.2em;
     }
     
     p {
-        color: #777;
+        color: #bbb;
     }
     
-    ul,
-    ol {
+    ul, ol {
         margin-top: 0;
         padding-left: 20px;
     }
     
-    ul li,
-    ol li {
+    ul li, ol li {
         margin-bottom: 10px;
     }
     
     pre {
-        background-color: #1e1e1e;
-        color: #ffffff;
+        background-color: var(--code-background-color);
+        color: var(--code-text-color);
         padding: 20px;
         border-radius: 5px;
         overflow-x: auto;
-      }
-      
-      pre code {
+    }
+    
+    pre code {
         display: block;
         white-space: pre;
-      }
-      
-      .keyword {
-        color: #569cd6;
-      }
-      
-      .string {
-        color: #ce9178;
-      }
-      
-      .comment {
-        color: #6a9955;
-      }
-      
-      .punctuation {
-        color: #d4d4d4;
-      }
-      
+    }
+    
+    .keyword {
+        color: var(--primary-color);
+    }
+    
+    .string {
+        color: var(--secondary-color);
+    }
+    
+    .comment {
+        color: #6c757d;
+    }
+    
+    .punctuation {
+        color: #6c757d;
+    }
     
     a {
         text-decoration: none;
-        color: #007bff;
+        color: var(--primary-color);
+        transition: color 0.3s ease;
     }
     
     a:hover {
+        color: var(--link-hover-color);
         text-decoration: underline;
-    }
+    }    
     </style>
     <body>
-        <h1>Documentación de la API de Generación de Códigos QR</h1>
-        <p>¡Bienvenido a la documentación de QRAPI! Esta API proporciona endpoints para generar códigos QR para una variedad de propósitos, incluyendo URL, vCard, Twitter, correo electrónico, SMS, WiFi, Bitcoin, archivos PDF, archivos MP3, la App Store y galerías de imágenes.</p>
-        <h2>Tecnologías Utilizadas</h2>
-        <p>
-            <a href="https://nodejs.org/"><img src="https://img.shields.io/badge/Node.js-14.x-green?logo=node.js" alt="Node.js"></a>
-            <a href="https://expressjs.com/"><img src="https://img.shields.io/badge/Express-4.x-blue" alt="Express"></a>
-            <a href="https://vercel.com/"><img src="https://img.shields.io/badge/Vercel-Deployed-black?logo=vercel" alt="Vercel"></a>
-            <a href="https://www.postman.com/"><img src="https://img.shields.io/badge/Postman-API%20Testing-orange?logo=postman" alt="Postman"></a>
-        </p>
-        <h2>Limitación de Solicitudes</h2>
-        <p>La API implementa un límite de solicitudes para prevenir un uso excesivo del servicio. Si excedes el límite de 1500 solicitudes por día, recibirás un mensaje de error.</p>
-        <p><strong>URL Base:</strong> <code>https://qrapi-rho.vercel.app</code></p>
+    <div class="container">
+        <h1>QR API</h1>
+        <p>Esta API proporciona endpoints para generar códigos QR para una variedad de propósitos, incluyendo URL, vCard, Twitter, correo electrónico, SMS, WiFi, Bitcoin, archivos PDF, archivos MP3, la App Store y galerías de imágenes.</p>
+        
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Tecnologías Utilizadas</h2>
+                <p>
+                    <a href="https://nodejs.org/"><img src="https://img.shields.io/badge/Node.js-14.x-green?logo=node.js" alt="Node.js"></a>
+                    <a href="https://expressjs.com/"><img src="https://img.shields.io/badge/Express-4.x-blue" alt="Express"></a>
+                    <a href="https://vercel.com/"><img src="https://img.shields.io/badge/Vercel-Deployed-black?logo=vercel" alt="Vercel"></a>
+                    <a href="https://www.postman.com/"><img src="https://img.shields.io/badge/Postman-API%20Testing-orange?logo=postman" alt="Postman"></a>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <h2>Limitación de Solicitudes</h2>
+                <p>La API implementa un límite de solicitudes para prevenir un uso excesivo del servicio. Si excedes el límite de 1500 solicitudes por día, recibirás un mensaje de error.</p>
+                <p><strong>URL Base:</strong> <code>https://qrapi-rho.vercel.app</code></p>
+            </div>
+        </div>
+
         <h2>Endpoints Disponibles</h2>
         <ol>
             <li>
@@ -272,12 +319,13 @@ app.get('/', (req, res) => {
                 </ul>
             </li>
         </ol>
-        
+
         <hr>
         <h2>Contacto</h2>
         <p>Dame una estrella en <a href="https://github.com/ManuFerrer094/qr-api">Github</a>!</p>
         <p>¡Conéctate conmigo en <a href="https://www.linkedin.com/in/manuferrer/">LinkedIn</a>!</p>
-    </body>
+    </div>
+</body>
     </html>    
     `;
 
